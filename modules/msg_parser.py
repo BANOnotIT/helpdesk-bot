3d12e06ef2158f41c6838017701b43a5807b2ae2
BANO.notIT <bano.notit@yandex.ru>
2019-01-02 22:21:41 +0300

    feat: Добавить независимость от отступов в .msg

--- modules/msg_parser.py
+++ modules/msg_parser.py
@@ -1,8 +1,8 @@
 def get_messages(text: str):
     result = dict()
     current_key = None
-    for line in text.split("\n"):
-
+    for no, line in enumerate(text.split("\n")):
+        line = line.strip()
         # Если строка является комментарием
         if line.startswith('#'):
             continue
@@ -13,16 +13,13 @@ def get_messages(text: str):
             if not line:
                 continue
             if not is_key:
-                raise ValueError('File must start with key name')
+                raise ValueError('File must start with key name. Line {}'.format(no))
 
         if is_key:
-            if result.get(current_key):
-                result[current_key] = result[current_key].strip()
-
             current_key = line[1:]
             result[current_key] = ''
             continue
 
-        result[current_key] += '\n' + line
+        result[current_key] = (result[current_key] + '\n' + line).strip()
 
     return result
